1c593d0c0fc5148c63547d533f6a5d3d18c8a8af
Rupansh Sekar <rupanshsekar@hotmail.com>
2020-02-26 17:10:04 +0530

    BoardConfig: Fix TARGET_ARCH_VARIANT
    Signed-off-by: Rupansh Sekar <rupanshsekar@hotmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ ALLOW_MISSING_DEPENDENCIES=true
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
